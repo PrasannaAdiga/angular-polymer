874e47acbd44c06b1217fcdca103584cd70aac9f
Prasanna Adiga <gpadiga.adiga@gmail.com>
2016-01-16 08:29:39 +0100

    initial commit - creating a TDD environment using generator-ng-poly

---
+++ app/app-module.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name app
+   * @description
+   *
+   */
+  angular
+    .module('app', [
+      'ui.router',
+      'home'
+    ]);
+}());
